80071f3150f744d11b60baa5ca52bf046d2004de
Titus <tituswormer@gmail.com>
2019-08-04 16:19:00 +0200

    Move humans, structure to `unifiedjs/governance`
    Closes GH-2.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>

---
+++ config/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
+// Name of the whole collective.
+exports.collective = 'unifiedjs'
+
+exports.humans = load('unified-humans')
+exports.teams = load('unified-teams')
+exports.ghOrgs = load('github-organizations')
+exports.ghTeams = load('github-teams')
+exports.ghHumans = load('github-humans')
+
+function load(name) {
+  return yaml.safeLoad(fs.readFileSync(path.join('config', name + '.yml')))
+}
