fbbbb09f158081e70c76e24df55abfa9858f029b
Titus Wormer <tituswormer@gmail.com>
2020-03-25 17:35:20 +0100

    Update dev-dependencies

--- script/crawl.js
+++ script/crawl.js
@@ -6,8 +6,10 @@ const base =
   'https://raw.githubusercontent.com/unifiedjs/collective/master/data/'
 const files = ['humans.yml', 'teams.yml']
 
-files.forEach(filename => {
-  https.get(base + filename, res => {
-    res.pipe(fs.createWriteStream(path.join('config', 'unified-' + filename)))
+files.forEach((filename) => {
+  https.get(base + filename, (response) => {
+    response.pipe(
+      fs.createWriteStream(path.join('config', 'unified-' + filename))
+    )
   })
 })
