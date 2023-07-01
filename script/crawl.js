d5d23cb38c17227efc8b23f2868a29ff65d0250f
Titus Wormer <tituswormer@gmail.com>
2023-07-01 11:20:00 +0200

    Remove `xo` rule

--- script/crawl.js
+++ script/crawl.js
@@ -1,14 +1,12 @@
-import fs from 'node:fs'
+import {createWriteStream} from 'node:fs'
 import path from 'node:path'
 import https from 'node:https'
 
 const base = 'https://raw.githubusercontent.com/unifiedjs/collective/HEAD/data/'
 const files = ['humans.yml', 'teams.yml']
 
-files.forEach((filename) => {
+for (const filename of files) {
   https.get(base + filename, (response) => {
-    response.pipe(
-      fs.createWriteStream(path.join('config', 'unified-' + filename))
-    )
+    response.pipe(createWriteStream(path.join('config', 'unified-' + filename)))
   })
-})
+}
